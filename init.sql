1d1a0d7dacbda022b07904ccf0269d84dbbc394a
alperbiceral <alperbiceral@gmail.com>
2025-06-16 15:52:18 +0300

    fixed initial data bugs

--- init.sql
+++ init.sql
@@ -57,45 +57,49 @@ VALUES
 ('godfather', 'pass123', 'godfather@email.com', 'guest'),
 ('sindirella', 'sindirella123', 'sindirella@email.com', 'guest');
 
-INSERT INTO `discussions` (`title`, `content`, `image_path`, `user_id`)
+INSERT INTO `discussions` (`title`, `content`, `image_path`, `user_id`, `post_count`)
 SELECT * FROM (SELECT
   'Let`s discuss' AS `title`,
   'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.' AS `content`,
   'images/lets_discuss.jpg' AS `image_path`,
-  1 AS `user_id`
+  1 AS `user_id`,
+  2 AS `post_count`
 ) AS `tmp`
 WHERE NOT EXISTS (
   SELECT 1 FROM `discussions` WHERE `title` = 'Let`s discuss' AND `user_id` = 1
 );
 
-INSERT INTO `discussions` (`title`, `content`, `image_path`, `user_id`)
+INSERT INTO `discussions` (`title`, `content`, `image_path`, `user_id`,  `post_count`)
 SELECT * FROM (SELECT
   'OMG What Happened!!' AS `title`,
   'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.' AS `content`,
   'images/omg.jpg' AS `image_path`,
-  1 AS `user_id`
+  1 AS `user_id`,
+  2 AS `post_count`
 ) AS `tmp`
 WHERE NOT EXISTS (
   SELECT 1 FROM `discussions` WHERE `title` = 'OMG What Happened!!' AND `user_id` = 1
 );
 
-INSERT INTO `discussions` (`title`, `content`, `image_path`, `user_id`)
+INSERT INTO `discussions` (`title`, `content`, `image_path`, `user_id`, `post_count`)
 SELECT * FROM (SELECT
   'BuggyVault is a nice place' AS `title`,
   'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.' AS `content`,
   'images/thumbs_up.png' AS `image_path`,
-  2 AS `user_id`
+  2 AS `user_id`,
+  2 AS `post_count`
 ) AS `tmp`
 WHERE NOT EXISTS (
   SELECT 2 FROM `discussions` WHERE `title` = 'BuggyVault is a nice place' AND `user_id` = 2
 );
 
-INSERT INTO `discussions` (`title`, `content`, `image_path`, `user_id`)
+INSERT INTO `discussions` (`title`, `content`, `image_path`, `user_id`, `post_count`)
 SELECT * FROM (SELECT
   'I`m hiring a driver' AS `title`,
   'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.' AS `content`,
   'images/driver.jpg' AS `image_path`,
-  3 AS `user_id`
+  3 AS `user_id`,
+  2 AS `post_count`
 ) AS `tmp`
 WHERE NOT EXISTS (
   SELECT 3 FROM `discussions` WHERE `title` = 'I`m hiring a driver' AND `user_id` = 3
@@ -104,7 +108,7 @@ WHERE NOT EXISTS (
 INSERT INTO `posts` (`content`, `image_path`, `discussion_id`, `user_id`)
 SELECT * FROM (SELECT
   'I think it is a good idea to discuss this topic.' AS `content`,
-  'images/thumbs_up.png' AS `content`,
+  'images/thumbs_up.png' AS `image_path`,
   1 AS `discussion_id`,
   1 AS `user_id`
 ) AS `tmp`
@@ -115,7 +119,7 @@ WHERE NOT EXISTS (
 INSERT INTO `posts` (`content`, `image_path`, `discussion_id`, `user_id`)
 SELECT * FROM (SELECT
   'I agree with you, let`s talk about it.' AS `content`,
-  'images/thumbs_up.png' AS `content`,
+  'images/thumbs_up.png' AS `image_path`,
   1 AS `discussion_id`,
   2 AS `user_id`
 ) AS `tmp`
@@ -126,7 +130,7 @@ WHERE NOT EXISTS (
 INSERT INTO `posts` (`content`, `image_path`, `discussion_id`, `user_id`)
 SELECT * FROM (SELECT
   'I think we should focus on the bugs first.' AS `content`,
-  'images/thumbs_up.png' AS `content`,
+  'images/thumbs_up.png' AS `image_path`,
   2 AS `discussion_id`,
   3 AS `user_id`
 ) AS `tmp`
@@ -137,7 +141,7 @@ WHERE NOT EXISTS (
 INSERT INTO `posts` (`content`, `image_path`, `discussion_id`, `user_id`)
 SELECT * FROM (SELECT
   'I have some ideas about the bugs.' AS `content`,
-  'images/thumbs_up.png' AS `content`,
+  'images/thumbs_up.png' AS `image_path`,
   2 AS `discussion_id`,
   1 AS `user_id`
 ) AS `tmp`
@@ -148,7 +152,7 @@ WHERE NOT EXISTS (
 INSERT INTO `posts` (`content`, `image_path`, `discussion_id`, `user_id`)
 SELECT * FROM (SELECT
   'I love this place!' AS `content`,
-  'images/thumbs_up.png' AS `content`,
+  'images/thumbs_up.png' AS `image_path`,
   3 AS `discussion_id`,
   2 AS `user_id`
 ) AS `tmp`
@@ -159,7 +163,7 @@ WHERE NOT EXISTS (
 INSERT INTO `posts` (`content`, `image_path`, `discussion_id`, `user_id`)
 SELECT * FROM (SELECT
   'I think we can make it even better!' AS `content`,
-  'images/thumbs_up.png' AS `content`,
+  'images/thumbs_up.png' AS `image_path`,
   3 AS `discussion_id`,
   3 AS `user_id`
 ) AS `tmp`
