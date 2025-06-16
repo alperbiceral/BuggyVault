b5b2079036553bf91bb1c910c523fe79cc404680
alperbiceral <alperbiceral@gmail.com>
2025-06-16 15:17:49 +0300

    added initial data to db

--- init.sql
+++ init.sql
@@ -50,3 +50,25 @@ CREATE TABLE IF NOT EXISTS `posts` (
     REFERENCES `buggyvault`.`users` (`id`)
     ON DELETE CASCADE
 );
+
+INSERT INTO `users` (`username`, `password`, `email`, `role`)
+VALUES
+('crazyboy', 'letmein', 'crazyboy@email.com', 'guest'),
+('godfather', 'pass123', 'godfather@email.com', 'guest'),
+('sindirella', 'sindirella123', 'sindirella@email.com', 'guest');
+
+INSERT INTO `discussions` (`title`, `content`, `image_path`, `user_id`)
+VALUES
+('Let`s discuss', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', "images/lets_discuss.jpg", 1),
+('OMG What Happened!!', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', "images/omg.jpg", 1),
+('BuggyVault is a nice place', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', "images/thumbs_up.png", 3);
+('I`m hiring a driver', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', "images/driver.jpg", 2);
+
+INSERT INTO `posts` (`content`, `image_path`, `discussion_id`, `user_id`)
+VALUES
+('I think it is a good idea to discuss this topic.', "images/thumbs_up.png", 1, 2),
+('I agree with you, let`s talk about it.', "images/thumbs_up.png", 1, 3),
+('I think we should focus on the bugs first.', "images/thumbs_up.png", 2, 1),
+('I have some ideas about the bugs.', "images/thumbs_up.png", 2, 3),
+('I love this place!', "images/thumbs_up.png", 3, 1),
+('I think we can make it even better!', "images/thumbs_up.png", 3, 2);
